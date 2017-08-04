1951281ba76a5d80ce332c709d0fcfaef926afc7
L Barbee <bluelb84@gmail.com>
2017-08-04 10:59:04 -0400

    edited thumbnail click event listener

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 // target thumbnail on click or keyboard event
 // change hero image src and alt with clicked thumbnail info
 
-$('.thumbnails .thumbnail').on('click', 'img', function(event){
-    event.stopPropagation();
-    $('.hero img').attr('src') === $(event.currentTarget).attr('src');
-    $('.hero img').attr('alt') === $(event.currentTarget).attr('alt');
-})
+function handleThumbnailClicks() {
+    $('.thumbnail').on('click', function(event) {
+        const imgSrc = $(event.currentTarget).find('img').attr('src');
+        const imgAlt = $(event.currentTarget).find('img').attr('alt');
+
+        $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
+    });
+}
+
+$(handleThumbnailClicks());
