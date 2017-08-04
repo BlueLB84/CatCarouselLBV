219d435702f6f9fa05605b37bd62ecc9d4cd1f43
L Barbee <bluelb84@gmail.com>
2017-08-04 10:50:05 -0400

    added event listener to app.js file

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-// this is filler text
+// target thumbnail on click or keyboard event
+// change hero image src and alt with clicked thumbnail info
+
+$('.thumbnails .thumbnail').on('click', 'img', function(event){
+    event.stopPropagation();
+    $('.hero img').attr('src') === $(event.currentTarget).attr('src');
+    $('.hero img').attr('alt') === $(event.currentTarget).attr('alt');
+})
